6cdd3db8edae4a7c0c2b44485bdf7d7b6e5dea4f
Rahavi Sirithar <157800741+RahaviSiri@users.noreply.github.com>
2024-10-30 19:18:24 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const db = require('./config/db');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: 'http://localhost:5176', 
+  origin: 'http://localhost:5173', 
   credentials: true 
 };
 
